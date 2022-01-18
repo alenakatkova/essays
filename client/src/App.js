03311d24508e6581e32ce1a941e5b4369947edd2
Alena Katkova <katkova.web@gmail.com>
2022-01-18 19:51:37 +0300

    delete create react app files

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-import logo from "./logo.svg";
 import axios from "axios";
 import "./App.css";
 
@@ -21,7 +20,6 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
