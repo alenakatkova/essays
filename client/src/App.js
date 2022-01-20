22ad6a96971c0aa0791bafa970462e12ec1cdbd2
Alena Katkova <katkova.web@gmail.com>
2022-01-20 17:21:59 +0300

    add prettier

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,7 @@
 import React from "react";
 import axios from "axios";
 import "./App.css";
-import {
-  Routes,
-  Route,
-  Link
-} from "react-router-dom";
+import { Routes, Route, Link } from "react-router-dom";
 import Essay from "./Essay";
 
 const apiUrl = "http://localhost:8080";
@@ -19,62 +15,60 @@ function App() {
 
   const loadPosts = async () => {
     const res = await axios.get(apiUrl + "/posts");
-    console.log(res.data.data.posts)
+    console.log(res.data.data.posts);
     setPosts(res.data.data.posts);
   };
 
   return (
-      <div>
-          <nav>
-            <ul>
-              <li>
-                <Link to='/'>Home</Link>
-              </li>
-              <li>
-                <Link to='/profile'>Personal account</Link>
-              </li>
-              <li>
-                <Link to='/feed'>Feed</Link>
-              </li>
-              <li>
-                <Link to='/essay'>Essay</Link>
-              </li>
-              <li>
-                <Link to='/my-essay'>My essay</Link>
-              </li>
-              <li>
-                <Link to='/writing-settings'>Writing settings</Link>
-              </li>
-              <li>
-                <Link to='/writing'>Writing</Link>
-              </li>
-              <li>
-                <Link to='/signup'>Sign up</Link>
-              </li>
-            </ul>
-          </nav>
+    <div>
+      <nav>
         <ul>
-          {posts.map(post => (
-              <li key={post._id}>body: {post.body}</li>
-          ))}
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/profile">Personal account</Link>
+          </li>
+          <li>
+            <Link to="/feed">Feed</Link>
+          </li>
+          <li>
+            <Link to="/essay">Essay</Link>
+          </li>
+          <li>
+            <Link to="/my-essay">My essay</Link>
+          </li>
+          <li>
+            <Link to="/writing-settings">Writing settings</Link>
+          </li>
+          <li>
+            <Link to="/writing">Writing</Link>
+          </li>
+          <li>
+            <Link to="/signup">Sign up</Link>
+          </li>
         </ul>
+      </nav>
+      <ul>
+        {posts.map((post) => (
+          <li key={post._id}>body: {post.body}</li>
+        ))}
+      </ul>
 
-          {/*<Header/>*/}
-          <Routes>
-            <Route path='/essay' element={<Essay />} />
-            <Route path='/my-essay' element={<Essay />} />
-            <Route path='/writing-settings' element={<Essay />} />
-            <Route path='/profile' element={<Essay />} />
-            <Route path='/feed' element={<Essay />} />
-            <Route path='/writing' element={<Essay />} />
-            <Route path='/signup' element={<Essay />} />
-            <Route path='/' element={<Essay />} />
-          </Routes>
-          {/*<Footer/>*/}
-      </div>
+      {/*<Header/>*/}
+      <Routes>
+        <Route path="/essay" element={<Essay />} />
+        <Route path="/my-essay" element={<Essay />} />
+        <Route path="/writing-settings" element={<Essay />} />
+        <Route path="/profile" element={<Essay />} />
+        <Route path="/feed" element={<Essay />} />
+        <Route path="/writing" element={<Essay />} />
+        <Route path="/signup" element={<Essay />} />
+        <Route path="/" element={<Essay />} />
+      </Routes>
+      {/*<Footer/>*/}
+    </div>
   );
 }
-
-
 
 export default App;
