e7d1cad56eaca7e70c5f2612af2bcd1ec9b878eb
Alena Katkova <katkova.web@gmail.com>
2022-02-21 23:39:29 +0300

    render mock bookmarks from the server

--- client/src/api/UserAPI.js
+++ client/src/api/UserAPI.js
@@ -40,25 +40,8 @@ export const getFavAuthorsEssays = async (userId) => {
 
 export const getBookmarkedEssays = async (userId) => {
   try {
-    //const res = await instance.get(`/users/${userId}/bookmarked-essays`);
-    //return res.data.data.essays;
-    return [
-      {
-        _id: "essay01",
-        topic: "Carte géographique",
-        user_id: "user01",
-        title: "Types de carte",
-        body:
-          "Les cartes géographiques peuvent être classée en trois types :\n" +
-          "\n" +
-          "Les cartes topographiques qui recensent et décrivent de façon précise et détaillée les éléments naturels du terrain, tel le relief, l’hydrographie en utilisant une symbologie conventionnelles : courbes de niveau, relief ombré, traits bleus pour représenter les cours d’eau. Dans ce type de carte, une attention particulière est accordée au positionnement précis des objets représentés.",
-        comments: [],
-        editSuggestionsComments: [],
-        writingSettings: {},
-        createdAt: "2022-02-19T20:25:59.949Z",
-        updatedAt: "2022-02-19T20:25:59.949Z",
-      },
-    ];
+    const res = await instance.get(`/users/${userId}/bookmarked-essays`);
+    return res.data.data.essays;
   } catch (e) {
     console.error(e);
   }
--- server/controllers/user.js
+++ server/controllers/user.js
@@ -59,6 +59,35 @@ exports.getFavAuthorsEssays = async (req, res, next) => {
 
 exports.getBookmarkedEssays = async (req, res, next) => {
   // TODO сделать айдишкой эссе айдишку бумарки, добавить коммент в каждое эссе
+  try {
+    const essays = [
+      {
+        _id: "essay01",
+        topic: "Carte géographique",
+        user_id: "user01",
+        title: "Types de carte+++",
+        body:
+          "Les cartes géographiques peuvent être classée en trois types :\n" +
+          "\n" +
+          "Les cartes topographiques qui recensent et décrivent de façon précise et détaillée les éléments naturels du terrain, tel le relief, l’hydrographie en utilisant une symbologie conventionnelles : courbes de niveau, relief ombré, traits bleus pour représenter les cours d’eau. Dans ce type de carte, une attention particulière est accordée au positionnement précis des objets représentés.",
+        comments: [],
+        editSuggestionsComments: [],
+        writingSettings: {},
+        createdAt: "2022-02-19T20:25:59.949Z",
+        updatedAt: "2022-02-19T20:25:59.949Z",
+      },
+    ];
+    res.status(200).json({
+      status: "success",
+      data: {
+        essays,
+      },
+    });
+  } catch (e) {
+    res.status(400).json({
+      status: "fail",
+    });
+  }
 };
 
 exports.getUsers = async (req, res) => {
