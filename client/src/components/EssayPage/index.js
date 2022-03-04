2285ba50e978a990603beb86f0b7afb01f9c04d4
Alena Katkova <katkova.web@gmail.com>
2022-03-04 15:09:56 +0100

    on essay page don't show rating input and like buttons if it's logged in user essay

--- client/src/components/EssayPage/index.js
+++ client/src/components/EssayPage/index.js
@@ -5,10 +5,24 @@ import EssayCard from "../common/EssayCard/index";
 import { useParams } from "react-router-dom";
 import { getOneEssay } from "../../api/EssayAPI";
 import CommentsTabs from "./CommentsTabs";
+import { useAuth } from "../../contexts/authProvider";
+import { useTranslation } from "react-i18next";
+import { getUserInfo } from "../../api/UserAPI";
 
 const EssayPage = () => {
   let { essayId } = useParams();
   const [essay, setEssay] = React.useState(null);
+  const [user, setUser] = React.useState({});
+  let auth = useAuth();
+
+  const getUserData = React.useCallback(async () => {
+    const data = await getUserInfo(auth.user);
+    setUser(data);
+  }, [auth.user]);
+
+  React.useEffect(() => {
+    getUserData();
+  }, [getUserData]);
 
   const getEssayFromServer = React.useCallback(async () => {
     const essayFromServer = await getOneEssay(essayId);
@@ -20,15 +34,17 @@ const EssayPage = () => {
     getEssayFromServer();
   }, [getEssayFromServer]);
 
-  // likes,
-  //     userEssays,
-  //     favouritesAuthors,
-
   return (
     <RequireAuth>
       <Container>
         <div className="mb-3">
-          {essay && <EssayCard essay={essay} isOpen={true} />}
+          {essay && (
+            <EssayCard
+              essay={essay}
+              isOpen={true}
+              userEssays={user.essays || []}
+            />
+          )}
         </div>
         <div>
           <CommentsTabs essayId={essayId} essayText={essay && essay.body} />
